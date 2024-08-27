dfa9181dd785bb5487a27976ef81c55ea8b558ac
AbrorbekQodirovdeveloper <abrorbekdev07@gmail.com>
2024-08-27 21:31:06 +0500

    reactapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
+
+import React,{useState,useEffect, useCallback} from "react";
 import './App.css';
 
+
 function App() {
+   let [url,setUrl] = useState("https://jsonplaceholder.typicode.com/users");
+    let [data,setData] = useState([]);
+
+     const fetchTrip = useCallback( async () => {
+         try {
+           const response = await fetch(url);
+           const data = await response.json();
+           setData(data);
+         } catch (error) {
+           console.error("Error fetching data:", error);
+         }
+
+     })
+
+
+     useEffect(() => {
+        fetchTrip()
+     },[url]);
+     console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                      <th>Email</th>
+                       <th>phone</th>
+                    <th>City</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+       data.map((item, index) => {
+         return (
+          <tr key={item.id}>
+          <td>{item.id}</td>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.phone}</td>
+          <td>{item.address.city}</td>
+      </tr>
+         );
+       })
+     }
+
+
+
+            </tbody>
+        </table>
+    </div>
+
     </div>
   );
 }
